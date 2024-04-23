5a96cd30caedddd8d1d6f3f45eef3c05f1e4659d
Cliffmeister <dev.clfm5t12@gmail.com>
2024-04-23 13:49:20 +0800

    Converted cards into for-each dynamic components

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,29 @@
 import {
-    //Initial Components
-    IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
-    IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
-    IonIcon, IonCol, IonGrid, IonRow,  IonButton, IonNavLink
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+  IonItemDivider,
+  IonSearchbar
 } from '@ionic/react';
+
 //Custom CSS
 import './home.css';
 
@@ -12,6 +32,29 @@ import { speedometerOutline,calculator,pencil} from 'ionicons/icons';
 
 //Additional Routes
 import ClickCounter from './clickcounter';
+
+const cardData = [
+  {
+    title: 'Click Counter',
+    icon: speedometerOutline,
+    subtitle: 'Applet #1',
+    link: '/clickcounter'
+  },
+  {
+    title: 'Calculator',
+    icon: calculator,
+    subtitle: 'Applet #2',
+    link: '/calculator'
+  },
+  {
+    title: 'To Do List',
+    icon: speedometerOutline,
+    subtitle: 'Applet #3',
+    link: '/todolist'
+  }
+  // Add more card data objects here as needed
+];
+
   
   const Home: React.FC = () => {
     return (
@@ -27,69 +70,25 @@ import ClickCounter from './clickcounter';
               <IonTitle size="large">Home</IonTitle>
             </IonToolbar>
           </IonHeader>
-
-          {/*Click counter card*/}
-          <IonCard href='/clickcounter' >
-            <IonCardHeader>
-              <IonCardTitle>
-                <IonGrid>
-                  <IonRow>
-                  <IonCol push=".75">
-                    <IonIcon className="home-card-icon"  icon={speedometerOutline} color="primary"></IonIcon>
-                  </IonCol>
-                  <IonCol pull='3'>
-                    <div className="home-card-title">Click Counter</div>
-                    <IonCardSubtitle>Applet #1 </IonCardSubtitle>
-                    {/*
-                    <IonNavLink routerDirection="forward" component={() => <ClickCounter />}>
-                      <IonButton>Navigate</IonButton>
-                    </IonNavLink>
-                    */}
-                  </IonCol>
+          {cardData.map((card, index) => (
+            <IonCard key={index} href={card.link}>
+              <IonCardHeader>
+                <IonCardTitle>
+                  <IonGrid>
+                    <IonRow>
+                      <IonCol push=".75">
+                        <IonIcon className="home-card-icon" icon={card.icon} color="primary"></IonIcon>
+                      </IonCol>
+                      <IonCol pull='3'>
+                        <div className="home-card-title">{card.title}</div>
+                        <IonCardSubtitle>{card.subtitle}</IonCardSubtitle>
+                      </IonCol>
                     </IonRow>
-                </IonGrid>
-              </IonCardTitle>
-            </IonCardHeader>
-          </IonCard>
-
-          {/*Click counter card*/}
-          <IonCard href="/">
-            <IonCardHeader>
-              <IonCardTitle>
-                <IonGrid>
-                  <IonRow>
-                  <IonCol push=".75">
-                    <IonIcon className="home-card-icon"  icon={calculator} color="primary"></IonIcon>
-                  </IonCol>
-                  <IonCol pull='3'>
-                    <div className="home-card-title">Calculator</div>
-                    <IonCardSubtitle>Applet #2 </IonCardSubtitle>
-                  </IonCol>
-                    </IonRow>
-                </IonGrid>
-              </IonCardTitle>
-            </IonCardHeader>
-          </IonCard>
-
-           {/*To do list card*/}
-           <IonCard href="/todolist">
-            <IonCardHeader>
-              <IonCardTitle>
-                <IonGrid>
-                  <IonRow>
-                  <IonCol push=".75">
-                    <IonIcon className="home-card-icon"  icon={pencil} color="primary"></IonIcon>
-                  </IonCol>
-                  <IonCol pull='3'>
-                    <div className="home-card-title">To Do List</div>
-                    <IonCardSubtitle>Applet #3 </IonCardSubtitle>
-                  </IonCol>
-                    </IonRow>
-                </IonGrid>
-              </IonCardTitle>
-            </IonCardHeader>
-          </IonCard>
-          
+                  </IonGrid>
+                </IonCardTitle>
+              </IonCardHeader>
+            </IonCard>
+          ))}
         </IonContent>
       </IonPage>
     );
