4a3f0afb78cca91a1cf650d13d05aa0399424bcb
Cliffmeister <dev.clfm5t12@gmail.com>
2024-04-30 13:43:48 +0800

    Updates

--- assets/ios.transition-legacy-BURE8QMw.js
+++ assets/ios.transition-legacy-BURE8QMw.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Cfwz-ppi.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-Dqae6DWN.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-CR571InU.js
+++ assets/keyboard2-legacy-CR571InU.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Cfwz-ppi.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-Dqae6DWN.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
