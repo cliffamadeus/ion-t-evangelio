1819cdb16f810cdc5597fd844086b730925f129f
hexedhorizon <dev.clfm5t12@gmail.com>
2024-05-03 07:56:27 +0800

    Updates

--- assets/ios.transition-legacy-f2kPa8Xv.js
+++ assets/ios.transition-legacy-f2kPa8Xv.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ayBabDdk.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-COoBZCmQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
