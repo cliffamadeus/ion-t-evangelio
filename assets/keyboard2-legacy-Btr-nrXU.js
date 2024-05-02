7c530170b6ad0f927677cb61ab81221dbcdf696a
Cliffmeister <dev.clfm5t12@gmail.com>
2024-05-02 16:07:29 +0800

    Updates

--- assets/keyboard2-legacy-Btr-nrXU.js
+++ assets/keyboard2-legacy-Btr-nrXU.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CTBMd1DJ.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-ayBabDdk.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
