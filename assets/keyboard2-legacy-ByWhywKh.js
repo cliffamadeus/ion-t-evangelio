b732eb342c1465d980521529a06b296a08aa2905
Cliffmeister <dev.clfm5t12@gmail.com>
2024-05-03 11:09:49 +0800

    Updates

--- assets/keyboard2-legacy-ByWhywKh.js
+++ assets/keyboard2-legacy-ByWhywKh.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CbtDo9GW.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-D5rlp_bw.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
