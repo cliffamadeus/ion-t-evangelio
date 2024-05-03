06603c082ed6655a053f857c96506c7f682a44e9
Cliffmeister <dev.clfm5t12@gmail.com>
2024-05-03 15:59:16 +0800

    Updates

--- assets/md.transition-legacy-BklGDKil.js
+++ assets/md.transition-legacy-BklGDKil.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BaxvBNTF.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-DmubUm6r.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
