1df1ab4f511e69e9dbd6755f3cc625b935c57ed2
Cliffmeister <dev.clfm5t12@gmail.com>
2024-05-03 15:04:16 +0800

    Updates

--- assets/ios.transition-legacy-g9fN6sAf.js
+++ assets/ios.transition-legacy-g9fN6sAf.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D5rlp_bw.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-BaxvBNTF.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
